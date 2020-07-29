1179650f2c95a9be45d3dde5df53e5393f45f546
chunanshan ma <machuanshan@gmail.com>
2020-07-29 15:44:25 +0800

    add notification channel

--- YouNew.AndroidApp/MainActivity.cs
+++ YouNew.AndroidApp/MainActivity.cs
@@ -14,16 +14,28 @@ namespace YouNew.AndroidApp
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
+            CreateNotificationChannel();
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
         }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        private void CreateNotificationChannel()
+        {
+            var notificationChannel = new NotificationChannel(
+                Constants.NotificationChannelId,
+                Resources.GetString(Resource.String.notification_channel_name),
+                NotificationImportance.Default);
+            var notificationManager = (NotificationManager)GetSystemService(NotificationService);
+            notificationManager.CreateNotificationChannel(notificationChannel);
         }
     }
 }
